e687cf1ffdf549655b6be1d6d46738ae525617dc
Rifqi Ramadhan <rifqyomp@gmail.com>
2022-08-24 23:31:07 +0700

    [README] documentation

--- src/main/java/com/enigma/loan_backend/utils/Utility.java
+++ src/main/java/com/enigma/loan_backend/utils/Utility.java
@@ -2,8 +2,6 @@ package com.enigma.loan_backend.utils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Utility {
 
